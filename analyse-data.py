397e4e1afc30110c8b4dc29ac5f6b938daaac176
leantony <chachaantony@gmail.com>
2018-08-11 19:37:15 +0300

    minor changes

--- analyse-data.py
+++ analyse-data.py
@@ -4,53 +4,62 @@ import json
 import operator
 from prettytable import PrettyTable
 
-filename = "output.json"
-template_src = "table_template.html"
 
-print("reading file {0}".format(filename))
+def read_dataset(filename):
+    print("reading file {0}".format(filename))
+    with open(filename, 'r') as f:
+        content = f.read()
+        inner = json.loads(content)
+    return inner
 
-with open(filename, 'r') as f:
-    content = f.read()
-    inner = json.loads(content)
 
-# root
-results = inner['results']
+def construct_baseline_report(results):
+    i = 0
+    t = PrettyTable(['JavaFile', 'BackendCalls', 'Size'])
+    for r in results:
+        fname = results[i]['fileNameWithPackageName']
+        bs = results[i]['backendCalls']
+        size = len(bs)
+        procs = '\n'.join(bs.keys())
+        i = i + 1
+        t.add_row([fname, procs, size])
+        # if i > 0:
+        #     break
 
-# metadata
-pathUsed = inner['rootPath']
-timeTaken = inner['timeTaken']
-resultsCount = inner['resultCount']
+    t.sortby = "Size"
+    t.reversesort = True
+    t.align["JavaFile"] = "l"
+    t.align["BackendCalls"] = "l"
 
-# now the table
-i = 0
-t = PrettyTable(['JavaFile', 'BackendCalls', 'Size'])
-for r in results:
-    fname = results[i]['fileNameWithPackageName']
-    bs = results[i]['backendCalls']
-    size = len(bs)
-    procs = '\n'.join(bs.keys())
-    i = i + 1
-    t.add_row([fname, procs, size])
-    # if i > 0:
-    #     break
+    html = t.get_html_string()
+    return html
 
-t.sortby = "Size"
-t.reversesort = True
-t.align["JavaFile"] = "l"
-t.align["BackendCalls"] = "l"
 
-html = t.get_html_string()
+def write_baseline_report(template_file, file_to_write, data_to_write, **kwargs):
+    with open(template_file, 'r') as f:
+        contents = f.read()
+        while '{doc_title}' in contents:
+            contents = contents.replace(
+                '{doc_title}', 'GIS source tree analysis')
+        while '{scanned_path}' in contents:
+            contents = contents.replace('{scanned_path}', kwargs['path'])
+        while '{file_count}' in contents:
+            contents = contents.replace(
+                '{file_count}', str(kwargs['count']))
+        while '{table_contents}' in contents:
+            contents = contents.replace('{table_contents}', data_to_write)
 
-with open(template_src, 'U') as f:
-    contents = f.read()
-    while '{doc_title}' in contents:
-        contents = contents.replace('{doc_title}', 'GIS source tree analysis')
-    while '{scanned_path}' in contents:
-        contents = contents.replace('{scanned_path}', inner['rootPath'])
-    while '{file_count}' in contents:
-        contents = contents.replace('{file_count}', str(inner['resultCount']))
-    while '{table_contents}' in contents:
-        contents = contents.replace('{table_contents}', html)
+        with open(file_to_write, "w") as f:
+            f.write(contents)
 
-with open('table.html', "w") as f:
-    f.write(contents)
+
+dataset_file = "output.json"
+template_file = "table_template.html"
+out_file = "table.html"
+
+data = read_dataset(dataset_file)
+html_output = construct_baseline_report(data['results'])
+
+args = {'path': data['rootPath'], 'count': data['resultCount']}
+print(args)
+write_baseline_report(template_file, out_file, html_output, **args)
