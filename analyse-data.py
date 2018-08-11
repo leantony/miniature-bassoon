b3e6a7400fa030a154d261b36dbc0fcd4b326656
leantony <chachaantony@gmail.com>
2018-08-11 14:04:27 +0300

    generate html

--- analyse-data.py
+++ analyse-data.py
@@ -5,6 +5,8 @@ import operator
 from prettytable import PrettyTable
 
 filename = "output.json"
+template_src = "table_template.html"
+
 print("reading file {0}".format(filename))
 
 with open(filename, 'r') as f:
@@ -19,22 +21,36 @@ pathUsed = inner['rootPath']
 timeTaken = inner['timeTaken']
 resultsCount = inner['resultCount']
 
-# r = PrettyTable(['Info', 'message'])
-# r.add_row(['Path', pathUsed])
-# r.add_row(['ScanTime', timeTaken])
-# r.add_row(['ResultsCount', resultsCount])
-
 # now the table
 i = 0
-t = PrettyTable(['JavaFile', 'BackendCalls'])
+t = PrettyTable(['JavaFile', 'BackendCalls', 'Size'])
 for r in results:
     fname = results[i]['fileNameWithPackageName']
-    backendCalls = results[i]['numberOfBackendCalls']
+    bs = results[i]['backendCalls']
+    size = len(bs)
+    procs = '\n'.join(bs.keys())
     i = i + 1
-    t.add_row([fname, backendCalls])
+    t.add_row([fname, procs, size])
+    # if i > 0:
+    #     break
 
-t.sortby = "BackendCalls"
+t.sortby = "Size"
 t.reversesort = True
 t.align["JavaFile"] = "l"
+t.align["BackendCalls"] = "l"
 
-print(t)
+html = t.get_html_string()
+
+with open(template_src, 'U') as f:
+    contents = f.read()
+    while '{doc_title}' in contents:
+        contents = contents.replace('{doc_title}', 'GIS source tree analysis')
+    while '{scanned_path}' in contents:
+        contents = contents.replace('{scanned_path}', inner['rootPath'])
+    while '{file_count}' in contents:
+        contents = contents.replace('{file_count}', str(inner['resultCount']))
+    while '{table_contents}' in contents:
+        contents = contents.replace('{table_contents}', html)
+
+with open('table.html', "w") as f:
+    f.write(contents)
